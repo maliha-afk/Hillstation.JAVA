afe21e84c098d2daee5914f21fefc3353cb66b19
maliha-afk <24amaliha@gmail.com>
2025-07-24 22:51:57 +0600

    Update Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter ANY number: ");
+       int n = sc.nextInt();
+       System.out.println("You entered: " + n);
+       System.out.println("Enter ANY word: ");
+       String s = sc.next();
+       System.out.println("You entered: " + s);
+    }
+    
+}
